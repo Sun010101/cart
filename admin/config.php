d4ed7a2330650d578a5711493bcb9485f0fbbc84
unknown <xiaoqi_sunny@163.com>
2018-12-02 11:35:20 +0800

    esbc

---
+++ admin/config.php
@@ -0,0 +1,33 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.ilovemyheart.org/cart/admin/');
+define('HTTP_CATALOG', 'http://www.ilovemyheart.org/cart/');
+// HTTPS
+define('HTTPS_SERVER', 'http://www.ilovemyheart.org/cart/admin/');
+define('HTTPS_CATALOG', 'http://www.ilovemyheart.org/cart/');
+// DIR
+define('DIR_APPLICATION', '/home/ilovemyheart/public_html/cart/admin/');
+define('DIR_SYSTEM', '/home/ilovemyheart/public_html/cart/system/');
+define('DIR_IMAGE', '/home/ilovemyheart/public_html/cart/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storager3li728tg11x/');
+define('DIR_CATALOG', '/home/ilovemyheart/public_html/cart/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'i3602736_op1');
+define('DB_PASSWORD', 'O.mjQhvY1MVqg4b5kXy68');
+define('DB_DATABASE', 'i3602736_op1');
+define('DB_PREFIX', 'op_');
+define('DB_PORT', '3306');
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.ilovemyheart.org/cart/');
+// HTTPS
+define('HTTPS_SERVER', 'http://www.ilovemyheart.org/cart/');
+// DIR
+define('DIR_APPLICATION', '/home/ilovemyheart/public_html/cart/catalog/');
+define('DIR_SYSTEM', '/home/ilovemyheart/public_html/cart/system/');
+define('DIR_IMAGE', '/home/ilovemyheart/public_html/cart/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storager3li728tg11x/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'i3602736_op1');
+define('DB_PASSWORD', 'O.mjQhvY1MVqg4b5kXy68');
+define('DB_DATABASE', 'i3602736_op1');
+define('DB_PREFIX', 'op_');
+define('DB_PORT', '3306');
